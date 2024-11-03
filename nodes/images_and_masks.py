79e674eb00b2503fa86e9fe50cc33e0fef5e965a
hben35096 <hben35096@gmail.com>
2024-11-04 02:08:18 +0800

    Efficiency optimization, new hybrid nodes

--- nodes/images_and_masks.py
+++ nodes/images_and_masks.py
@@ -2,6 +2,7 @@ import numpy as np
 import torch
 from PIL import Image
 import cv2
+# 基本完成
 
 # from was-node-suite-comfyui
 def tensor2pil(image):
@@ -61,9 +62,10 @@ def adjust_levels(image:Image, input_black:int=0, input_white:int=255, midtones:
     img = img.astype(np.uint8)
     return cv22pil(img)
 
-###################################################
 
-class BatchImageBlend:
+# ##################################################
+
+class RN_BatchImageBlend:
     def __init__(self):
         pass
 
@@ -71,9 +73,64 @@ class BatchImageBlend:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "images": ("IMAGE",),  # 接收多批次的图像输入
-                "opacity": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),  # 透明度
+                "images": ("IMAGE",),  
+                "opacity": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),  # 不透明度
             },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "batch_image_blend"
+
+    CATEGORY = "Replenish/Image"
+
+    def batch_image_blend(self, images, opacity):
+        print("input image number:", len(images))
+
+        max_height = max(image.shape[0] for image in images)
+        max_width = max(image.shape[1] for image in images)
+        print(f"Max width: {max_width}; Max height:, {max_height}")
+
+        # 创建一个透明背景的输出图像
+        output_image = Image.new("RGBA", (max_width, max_height), (255, 0, 0, 0))
+
+        for image in images:
+            # print("input tensor space:", image.shape) # 调试
+            img = tensor2pil(image)
+            img = img.convert("RGBA") 
+
+            # 计算中心位置
+            
+            alpha_channel = img.split()[-1]  # 获取图像的透明通道
+            alpha_channel = Image.eval(alpha_channel, lambda a: int(a * opacity))  # 将原有透明度与 blend_percentage 相乘
+
+            # 替换图像的透明通道
+            img.putalpha(alpha_channel)
+
+            output_image = Image.alpha_composite(output_image, img)
+
+        output_tensor = pil2tensor(output_image)
+        # print("Output tensor shape:", output_tensor.shape) # 调试
+
+        return (output_tensor,)
+    
+
+class RN_MultipleImageBlend:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image_a": ("IMAGE",),  
+                "opacity": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),  # 不透明度
+            },
+            "optional": {
+                "image_b": ("IMAGE",), 
+                "image_c": ("IMAGE",), 
+                "image_d": ("IMAGE",), 
+            }
         }
 
     RETURN_TYPES = ("IMAGE",)
@@ -82,45 +139,73 @@ class BatchImageBlend:
 
     CATEGORY = "Replenish/Image"
 
-    def image_blend(self, images, opacity):
-        # 找到最大高度和宽度
-        max_height = max(image.shape[0] for image in images)
-        max_width = max(image.shape[1] for image in images)
-        print(f"Max width: {max_width}; Max height:, {max_height}")
-        
-        # 创建一个空白输出图像
-        output_image = Image.new("RGBA", (max_width, max_height), (0, 0, 0, 0))
+    def image_blend(self, image_a, opacity, image_b=None, image_c=None, image_d=None):
+        # 创建 images 列表并添加非空的图像
+        images = [image_a]
+        if image_b is not None:
+            images.append(image_b)
+        if image_c is not None:
+            images.append(image_c)
+        if image_d is not None:
+            images.append(image_d)
+
+        print("input image number:", len(images))
+
+        max_height = max(image.shape[1] for image in images)
+        max_width = max(image.shape[2] for image in images)
+        print(f"Max width: {max_width}; Max height: {max_height}")
+
+        # 创建一个透明背景的输出图像的 NumPy 数组
+        output_image = np.zeros((max_height, max_width, 4), dtype=np.float32)
 
         for image in images:
-            print("input tensor space:", image.shape)
-            img = tensor2pil(image)
-            img = img.convert("RGBA")  # 确保图像是RGBA模式
+            # 转换张量到 PIL 图像并确保是 RGBA 模式
+            img = tensor2pil(image).convert("RGBA")
+            img = np.array(img, dtype=np.float32)  # 转换为 NumPy 数组以便操作
+
+            # 计算中心位置
+            x_pos = (max_width - img.shape[1]) // 2
+            y_pos = (max_height - img.shape[0]) // 2
             
-            # 计算位置，将图像中心对齐
-            x_pos = (max_width - img.width) // 2
-            y_pos = (max_height - img.height) // 2
-            
-            # 创建新图像以保存透明度处理后的结果
-            img_with_opacity = Image.new("RGBA", img.size)
+            # 调用 blend_layers 函数叠加图像
+            output_image[y_pos:y_pos+img.shape[0], x_pos:x_pos+img.shape[1]] = \
+                self.blend_layers(output_image[y_pos:y_pos+img.shape[0], x_pos:x_pos+img.shape[1]], img, opacity)
 
-            for x in range(img.width):
-                for y in range(img.height):
-                    r, g, b, a = img.getpixel((x, y))
-                    new_alpha = int(a * opacity)  # 乘以原始透明度
-                    img_with_opacity.putpixel((x, y), (r, g, b, new_alpha))
+        # 将结果转换为 PIL 图像并转换为张量格式
+        output_image_pil = Image.fromarray(np.clip(output_image, 0, 255).astype(np.uint8), 'RGBA')
+        output_tensor = pil2tensor(output_image_pil)
 
-            # 将图像粘贴到输出图像
-            output_image.paste(img_with_opacity, (x_pos, y_pos), img_with_opacity)
+        return (output_tensor,)
 
-        # 打印输出图像的尺寸和张量形状
-        output_tensor = pil2tensor(output_image)
-        print("Output tensor shape:", output_tensor.shape)  # 输出张量的形状
+    # @staticmethod
+    def blend_layers(self, base, overlay, opacity):
+        alpha_overlay = overlay[..., 3] * opacity / 255.0  # 叠加层的透明度（0到1）
+        alpha_base = base[..., 3] / 255.0  # 底层的透明度（0到1）
 
-        return (output_tensor, )
+        # 最终的透明度
+        out_alpha = alpha_overlay + alpha_base * (1 - alpha_overlay)
+
+        # 避免除以零的情况
+        out_alpha_safe = np.where(out_alpha == 0, 1, out_alpha)
+
+        # 混合 RGB 通道
+        for c in range(3):  # R, G, B channels
+            base[..., c] = np.where(
+                out_alpha == 0,
+                base[..., c],  # 如果 `out_alpha` 为零，保持底层像素的值
+                (overlay[..., c] * alpha_overlay + base[..., c] * alpha_base * (1 - alpha_overlay)) / out_alpha_safe
+            )
+
+        base[..., 3] = out_alpha * 255  # 更新 alpha 通道
+
+        return base
 
 
-# 遮罩色阶调节
-class MaskLevelsAdjust:
+    
+
+# 可以了，准备增加对齐3种方法 完成
+# 准备分离对齐方法，
+class RN_ImageBlendBG:
     def __init__(self):
         pass
 
@@ -128,14 +213,209 @@ class MaskLevelsAdjust:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "masks": ("MASK",),  # 修改为处理遮罩
+                "image_bg": ("IMAGE",),
+                "image": ("IMAGE",),
+                "align": (["size", "top left", "center"],),
+                "opacity": ("FLOAT", {"default": 0.5, "min": 0.0, "max": 1.0, "step": 0.01}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "get_image"
+
+    CATEGORY = "Replenish/Image"
+
+    def get_image(self, image_bg, image, align, opacity):
+        # print("背景图片空间:", image_bg.shape) # 调试
+        
+        image_bg_pil = tensor2pil(image_bg).convert("RGBA")
+        img_pil = tensor2pil(image).convert("RGBA")
+        
+        output_image_pil = self.image_blend(image_bg_pil, img_pil, align, opacity)
+
+        output_tensor = pil2tensor(output_image_pil)
+
+        # 返回最终图像
+        return (output_tensor,)
+        
+    def image_blend(self, image_bg_pil, img_pil, align, opacity):
+        # 获取图像的尺寸
+        bg_width, bg_height = image_bg_pil.size
+        # print(f"背景图像尺寸: 宽度={bg_width}, 高度={bg_height}") # 调试
+
+        img_width, img_height = img_pil.size
+        # print(f"前景图像原始尺寸: 宽度={img_width}, 高度={img_height}") # 调试
+
+        # 调整前景图像尺寸以匹配背景图像尺寸
+        if align == "size":
+            aspect_ratio = img_width / img_height
+            bg_aspect_ratio = bg_width / bg_height
+
+            if aspect_ratio > bg_aspect_ratio:
+                new_height = bg_height
+                new_width = int(bg_height * aspect_ratio)
+            else:
+                new_width = bg_width
+                new_height = int(bg_width / aspect_ratio)
+
+            img_pil = img_pil.resize((new_width, new_height), Image.LANCZOS)
+
+            new_img = Image.new("RGBA", (bg_width, bg_height), (0, 0, 0, 0))
+            new_img.paste(img_pil, ((bg_width - new_width) // 2, (bg_height - new_height) // 2))
+            img_pil = new_img
+
+        elif img_width < bg_width or img_height < bg_height:
+            new_img = Image.new("RGBA", (bg_width, bg_height), (0, 0, 0, 0))
+            if align == "center":
+                new_img.paste(img_pil, ((bg_width - img_width) // 2, (bg_height - img_height) // 2))
+            elif align == "top left":
+                new_img.paste(img_pil, (0, 0))
+            img_pil = new_img
+        else:
+            if align == "center":
+                left = (img_width - bg_width) // 2
+                top = (img_height - bg_height) // 2
+            elif align == "top left":
+                left = 0
+                top = 0
+            img_pil = img_pil.crop((left, top, left + bg_width, top + bg_height))
+
+        # 确保前景图与背景图尺寸完全一致
+        if img_pil.size != image_bg_pil.size:
+            img_pil = img_pil.resize(image_bg_pil.size, Image.LANCZOS)
+            # print(f"前景图像尺寸已手动调整为背景图像尺寸: {img_pil.size}") # 调试
+
+        # 调整透明度
+        alpha_channel = img_pil.split()[-1]
+        alpha_channel = Image.eval(alpha_channel, lambda a: int(a * opacity))
+        img_pil.putalpha(alpha_channel)
+
+        # 合成图像
+        output_image_pil = Image.alpha_composite(image_bg_pil, img_pil)
+
+        return output_image_pil
+
+        
+class RN_MultipleImageBlend_2:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "image_a": ("IMAGE",),  
+                "opacity": ("FLOAT", {"default": 1.0, "min": 0.0, "max": 1.0, "step": 0.01}),  # 不透明度
+                "align": (["size", "top left", "center"],),  # 添加 align 参数
+            },
+            "optional": {
+                "image_b": ("IMAGE",), 
+                "image_c": ("IMAGE",), 
+                "image_d": ("IMAGE",), 
+            }
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "multiple_image_blend"
+
+    CATEGORY = "Replenish/Image"
+
+    def multiple_image_blend(self, image_a, opacity, align, image_b=None, image_c=None, image_d=None):
+        # 创建 images 列表并添加非空的图像
+        images = [image_a]
+        if image_b is not None:
+            images.append(image_b)
+        if image_c is not None:
+            images.append(image_c)
+        if image_d is not None:
+            images.append(image_d)
+
+        print("Input image number:", len(images))
+        max_height = max(image.shape[1] for image in images)
+        max_width = max(image.shape[2] for image in images)
+        print(f"Max width: {max_width}; Max height: {max_height}")
+        
+        image_bg_pil = Image.new("RGBA", (max_width, max_height), (255, 0, 0, 0)) # 创建一个透明背景的输出图像
+
+        blender = RN_ImageBlendBG() # 实例化 RN_ImageBlendBG
+
+        for image in images:
+            img_pil = tensor2pil(image).convert("RGBA")
+
+            image_bg_pil = blender.image_blend(image_bg_pil, img_pil, align, opacity) # 使用实例调用 image_blend 方法进行批处理
+
+        output_tensor = pil2tensor(image_bg_pil)
+        return (output_tensor,)
+
+
+    
+# 这个基本功能完成了，需要让它更灵活，还要做成批处理 完成
+class RN_FillAlpha:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "images": ("IMAGE",),
+                "fill_color": ("STRING", {"default": "0,0,0"}),
+            },
+        }
+
+    RETURN_TYPES = ("IMAGE",)
+    RETURN_NAMES = ("image",)
+    FUNCTION = "fill_alpha"
+    CATEGORY = "Replenish/Image"
+
+    def fill_alpha(self, images, fill_color):
+        # 解析填充颜色
+        if fill_color.startswith('#'):
+            hex_color = fill_color.lstrip('#')
+            fill_color = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+        else:
+            fill_color = tuple(map(int, fill_color.split(',')))
+
+        output_tensors = []
+        for image in images:
+            # print("图片空间:", image.shape) # 调试
+
+            # 创建RGBA背景图像
+            height, width = image.shape[0], image.shape[1]
+            bg_image = Image.new("RGBA", (width, height), (*fill_color, 255))
+
+            image = tensor2pil(image).convert("RGBA")
+
+            output_image = Image.alpha_composite(bg_image, image).convert("RGB") # 合成并转换为RGB模式
+
+            # 转换为张量并存储
+            output_tensor = pil2tensor(output_image)
+            output_tensors.append(output_tensor)
+
+        # 返回批量图像张量
+        batch_output_tensor = torch.cat(output_tensors, dim=0)
+        return (batch_output_tensor,)
+    
+    
+# 遮罩色阶调节
+class RN_MaskLevelsAdjust:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "masks": ("MASK",), 
                 "output_black_point": ("INT", {"default": 0, "min": 0, "max": 255, "step": 1, "display": "slider"}),
                 "output_white_point": ("INT", {"default": 255, "min": 0, "max": 255, "step": 1, "display": "slider"}),
             },
         }
 
     RETURN_TYPES = ("MASK",)
-    RETURN_NAMES = ("masks",)  # 修改返回名称为“masks”
+    RETURN_NAMES = ("masks",) 
     FUNCTION = 'levels'
     CATEGORY = "Replenish/Masks"
 
@@ -159,14 +439,17 @@ class MaskLevelsAdjust:
         # 将所有处理后的遮罩合并为一个张量
         result_tensor = torch.cat(ret_masks, dim=0)
 
-        # 打印输出信息
         print("Number of masks processed:", len(ret_masks))
-        print("Shape of the result tensor:", result_tensor.shape)
+        # print("Shape of the result tensor:", result_tensor.shape) # 调试
 
         return (result_tensor,)
-
-
+    
+    
 NODE_CLASS_MAPPINGS = {
-    "Batch Image Blend": BatchImageBlend,
-    "Mask Levels Adjust": MaskLevelsAdjust,
+    "Batch Image Blend": RN_BatchImageBlend,
+    "Multiple Image Blend": RN_MultipleImageBlend,
+    "Image Blend BG": RN_ImageBlendBG,
+    "Fill Alpha": RN_FillAlpha,
+    "Mask Levels Adjust": RN_MaskLevelsAdjust,
+    "RN_MultipleImageBlend_2": RN_MultipleImageBlend_2,
 }
